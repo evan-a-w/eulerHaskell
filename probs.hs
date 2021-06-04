7b9381dc02e71a36e02da5c74dc984fbf165e3eb
Evan Williams <evanawilliams@live.com>
2021-06-04 13:24:45 +1000

    prob19

--- probs.hs
+++ probs.hs
@@ -37,8 +37,8 @@ sol4 = maximum $ filter isPali $ [x*y | x <- [100..999], y <- [100..999]]
   where isPali :: Int -> Bool
         isPali i = (reverse . show) i  == show i
 
-sol4_2 :: Int
-sol4_2 = head $ dropWhile (not . isPali) $ reverse $ sort [x*y | x <- [100..999], y <- [100..999]]
+sol4' :: Int
+sol4' = head $ dropWhile (not . isPali) $ reverse $ sort [x*y | x <- [100..999], y <- [100..999]]
   where isPali :: Int -> Bool
         isPali i = (reverse . show) i  == show i
 
@@ -184,3 +184,192 @@ movements y x arr = if val /= 0
 sol15 = fst $ movements 0 0 vGrid
 
 sol16 = sum $ integerToList (2^1000)
+
+oneToNine = V.fromList
+            ["one", "two", "three", "four", "five", "six", "seven", "eight",
+             "nine"]
+tenToTwenty = V.fromList
+              ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+               "seventeen", "eighteen", "nineteen"]
+tenToNinety = V.fromList
+              ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+               "eighty", "ninety"]
+
+numToWord :: Integer -> String
+numToWord i = listWord $ integerToList i
+  where listWord l =
+              case length l of
+                1         -> oneToNine ! ((l!!0)-1)
+                2         -> if l!!1 == 0
+                                then tenToNinety ! ((l!!0)-1)
+                                else if l!!0 /= 1
+                                     then tenToNinety ! ((l!!0)-1) ++ " " ++ 
+                                          oneToNine ! ((l!!1)-1)
+                                     else tenToTwenty ! ((l!!1)-1)
+                3         -> let b = oneToNine ! ((l!!0)-1) ++ " hundred" in
+                              if l!!1 == 0 && l!!2 == 0
+                                 then b
+                                 else b ++ " and " ++ listWord (if l!!1 == 0
+                                                        then [l!!2]
+                                                        else [l!!1, l!!2])
+                otherwise -> "one thousand"
+
+wordToNine   = V.fromList [0,3,3,5,4,4,3,5,5,4]
+wordToTwenty = V.map (fromIntegral . length) tenToTwenty
+wordToNinety = V.fromList [0,3,6,6,5,5,5,7,6,6]
+
+countLetters :: Integer -> Integer
+countLetters i = listWord $ integerToList i
+  where listWord l =
+              case length l of
+                1         -> wordToNine ! (l!!0)
+                2         -> if l!!1 == 0
+                                then wordToNinety ! (l!!0)
+                                else if l!!0 /= 1
+                                     then wordToNinety ! (l!!0) + 
+                                          wordToNine ! (l!!1)
+                                     else wordToTwenty ! ((l!!1)-1)
+                3         -> let b = (wordToNine ! (l!!0)) + 7 in
+                              if l!!1 == 0 && l!!2 == 0
+                                 then b
+                                 else b + 3 + listWord [l!!1, l!!2]
+                otherwise -> 11
+
+sol17 = sum $ map countLetters [1..1000]
+-- compiler is amazing... sol17' is just as fast
+sol17' = sum $ map (sum . (map length) . words . numToWord) [1..1000]
+
+tri :: Vector (Vector Integer)
+tri = V.fromList $ map V.fromList 
+                 [ [75]
+                 , [95, 64]
+                 , [17, 47, 82]
+                 , [18, 35, 87, 10]
+                 , [20, 04, 82, 47, 65]
+                 , [19, 01, 23, 75, 03, 34]
+                 , [88, 02, 77, 73, 07, 63, 67]
+                 , [99, 65, 04, 28, 06, 16, 70, 92]
+                 , [41, 41, 26, 56, 83, 40, 80, 70, 33]
+                 , [41, 48, 72, 33, 47, 32, 37, 16, 94, 29]
+                 , [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14]
+                 , [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57]
+                 , [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48]
+                 , [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31]
+                 , [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23] ]
+
+startMap :: Map (Int, Int) Integer 
+startMap = Map.fromList [((i,14), (tri!14)!i) | i <- [0..14]]
+
+maximumPathSum :: (Int, Int) -> Map (Int, Int) Integer -> Integer
+maximumPathSum (x,y) m = fst $ go (x,y) m
+  where go (x, 14) m = ((tri!14)!x, m)
+        go (x, 13) m = let cv = (tri!13)!x
+                           fs = (tri!14)!x
+                           sn = (tri!14)!(x+1) in
+                             if fs > sn
+                                then (cv+fs, Map.insert (x,13) (cv+fs) m)
+                                else (cv+sn, Map.insert (x,13) (cv+sn) m)
+        go (x,y) m = case Map.lookup (x,y) m of
+                       Just v  -> (v, m)
+                       Nothing -> let cv = (tri!y)!x
+                                      (p1, m1) = go (x,y+1) m
+                                      (p2, m2) = go (x+1,y+1) m1 in
+                                    if p2 > p1
+                                       then (cv+p2, Map.insert (x,y) (cv+p2) m2)
+                                       else (cv+p1, Map.insert (x,y) (cv+p1) m2)
+
+sol18 = maximumPathSum (0,0) startMap
+
+isLeapYear :: Integral a => a -> Bool
+isLeapYear y
+  | y `mod` 100 == 0 = if y `mod` 400 == 0
+                          then True
+                          else False
+  | y `mod` 4 == 0   = True
+  | otherwise        = False
+
+data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
+  deriving (Eq, Show, Ord)
+data WeekDay = Mon | Tue | Wed | Thur | Fri | Sat | Sun
+  deriving (Eq, Ord, Show)
+
+instance Enum Month where
+  toEnum i = case i of
+               1         -> Jan
+               2         -> Feb
+               3         -> Mar
+               4         -> Apr
+               5         -> May
+               6         -> Jun
+               7         -> Jul
+               8         -> Aug
+               9         -> Sep
+               10        -> Oct
+               11        -> Nov
+               12        -> Dec
+               otherwise -> toEnum ((i `mod` 12) + 1)
+  fromEnum d = case d of
+                 Jan -> 1
+                 Feb -> 2
+                 Mar -> 3
+                 Apr -> 4
+                 May -> 5
+                 Jun -> 6
+                 Jul -> 7
+                 Aug -> 8
+                 Sep -> 9
+                 Oct -> 10
+                 Nov -> 11
+                 Dec -> 12
+
+instance Enum WeekDay where
+  toEnum i = case i of
+               1         -> Mon
+               2         -> Tue
+               3         -> Wed
+               4         -> Thur
+               5         -> Fri
+               6         -> Sat
+               7         -> Sun
+               otherwise -> toEnum ((i `mod` 8) + 1)
+  fromEnum d = case d of
+                 Mon  -> 1
+                 Tue  -> 2
+                 Wed  -> 3
+                 Thur -> 4
+                 Fri  -> 5
+                 Sat  -> 6
+                 Sun  -> 7
+
+type Year = Int
+type Day = Int
+
+data Date = Date Day Month Year WeekDay
+  deriving (Eq, Ord, Show)
+
+next :: Date -> Date
+next (Date d m y wd) = if d + 1 > days m y
+                          then if m == Dec
+                                  then Date 1 Jan (y+1) (succ wd)
+                                  else Date 1 (succ m) y (succ wd)
+                          else Date (d+1) m y (succ wd)
+
+days :: Month -> Year -> Int
+days m y
+  | m == Feb                    = if isLeapYear y
+                                     then 29
+                                     else 28
+  | elem m [Sep, Apr, Jun, Nov] = 30
+  | otherwise                   = 31
+
+firstJan1901 :: Date
+firstJan1901 = go (Date 1 Jan 1900 Mon)
+  where go r@(Date 1 Jan 1901 _) = r
+        go d = go $ next d
+
+sol19 :: Int
+sol19 = go firstJan1901
+  where go :: Date -> Int
+        go (Date 31 Dec 2000 _) = 0
+        go c@(Date 1 _ _ Sun) = 1 + (go $ next c)
+        go c = go $ next c
